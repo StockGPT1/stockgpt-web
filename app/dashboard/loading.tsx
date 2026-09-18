c4f1b45089b51a90b0b1331f7455da8689d0cfa7
StockGPT1 <contact.stockgpt@gmail.com>
2026-09-18 20:35:44 +0100

    Use animated StockGPT boot logo on dashboard load

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,5 +1,5 @@
-import { BrandLoaderPage } from "@/components/BrandLoader";
+import { StartupLogoLoaderPage } from "@/components/BrandLoader";
 
 export default function Loading() {
-  return <BrandLoaderPage label="Pulling up your dashboard" background="#072116" />;
+  return <StartupLogoLoaderPage />;
 }
