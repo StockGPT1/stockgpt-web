0dd8b7ef4df3e54030690cc4e0e0f8417a49eed2
StockGPT1 <contact.stockgpt@gmail.com>
2026-09-18 19:42:49 +0100

    Match signup loader styling

--- app/signup/loading.tsx
+++ app/signup/loading.tsx
@@ -1,5 +1,5 @@
 import { BrandLoaderPage } from "@/components/BrandLoader";
 
 export default function Loading() {
-  return <BrandLoaderPage label="Reserving your seat" background="#020806" />;
+  return <BrandLoaderPage label="Reserving your seat" />;
 }
