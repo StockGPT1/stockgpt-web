845416614785b6ca0de4a8a10357c7cba3bffc49
StockGPT1 <contact.stockgpt@gmail.com>
2026-09-18 19:42:56 +0100

    Match password update loader styling

--- app/update-password/loading.tsx
+++ app/update-password/loading.tsx
@@ -1,5 +1,5 @@
 import { BrandLoaderPage } from "@/components/BrandLoader";
 
 export default function Loading() {
-  return <BrandLoaderPage label="Securing your account" background="#020806" />;
+  return <BrandLoaderPage label="Securing your account" />;
 }
